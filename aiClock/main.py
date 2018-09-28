e097d2a3bd5faa903590fd854fd2f19dcccf5c18
xwatson <xwatson@foxmail.com>
2018-09-28 15:22:53 +0800

    优化代码

--- aiClock/main.py
+++ aiClock/main.py
@@ -6,28 +6,45 @@ from playAudio import play
 
 class myTherad(threading.Thread):
     def run(self):
-        # 百度地图api
+        # 获取路线
+        route = self.getBaiduLbs()
+        # 转换语音
+        ttsr = self.getXFTTS(route)
+        # 播放语音
+        self.playAudio(ttsr)
+        """ while True:
+            time.sleep(1)
+            self.getBaiDuRouteMatrix() """
+    def getBaiduLbs(self):
+         # 获取百度驾车路线
         baiduLbs = lbs.Lbs()
-        route = baiduLbs.getBaiDuRouteMatrix()
+        return baiduLbs.getBaiDuRouteMatrix()
+    def getXFTTS(self, route):
+        # 讯飞语音合成
         if not route is None:
             # 讯飞语音api
             xftts = tts.XFtts()
             ttsr = xftts.requestTTS(route)
-            # 播放音频
+
+            return { "result": ttsr, "aue": xftts.aue }
+        else:
+            return None
+    def playAudio(self, ttsr):
+        # 播放音频
+        if not ttsr is None:
             pAudio = play.PlayAudio()
-            contentType = ttsr.headers['Content-Type']
+            contentType = ttsr['result'].headers['Content-Type']
             if contentType == "audio/mpeg":
-                sid = ttsr.headers['sid']
-                if xftts.aue == "raw":
-                    pAudio.playAudio("audios/"+sid+".wav", ttsr.content)
+                sid = ttsr['result'].headers['sid']
+                if ttsr['aue'] == "raw":
+                    pAudio.playAudio("audios/"+sid+".wav", ttsr['result'].content)
                 else :
-                    pAudio.playAudio("audios/"+sid+".mp3", ttsr.content)
+                    pAudio.playAudio("audios/"+sid+".mp3", ttsr['result'].content)
                 print("success, sid = " + sid)
             else :
-                print(ttsr.text )
-        """ while True:
-            time.sleep(1)
-            self.getBaiDuRouteMatrix() """
+                print(ttsr['result'].text )
+        else:
+            print('错误：未获取到语音合成数据！')
 
 if __name__ == '__main__':
     ma = myTherad()
